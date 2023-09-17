409fc6a1e1aa8d67ca755eeedebb3c634f2e83de
Alvina Tahir <78271040+alvina-byte@users.noreply.github.com>
2023-09-17 17:29:18 +0500

    Add files via upload
    the backend needs work still.

---
+++ my-backend/server.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const app = express();
+const PORT = 4000;
+
+app.get('/', (req, res) => {
+  res.send('Hello, world!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+// To add a new task
+app.post('/addTask', (req, res) => {
+  // Logic to add a task
+  res.status(201).send('Task added');
+});
+
+// To delete a task
+app.delete('/deleteTask/:taskId', (req, res) => {
+  const taskId = req.params.taskId;
+  // Logic to delete a task
+  res.status(200).send(`Task ${taskId} deleted`);
+});
+
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('./tasks.db');
+
+db.serialize(() => {
+  db.run("CREATE TABLE IF NOT EXISTS tasks (id INTEGER PRIMARY KEY AUTOINCREMENT, task TEXT)");
+});
+
+app.post('/addTask', (req, res) => {
+  const task = req.body.task;
+  const sql = "INSERT INTO tasks (task) VALUES (?)";
+
+  db.run(sql, [task], function(err) {
+    if (err) {
+      return res.status(500).send(err.message);
+    }
+    return res.status(201).send({ id: this.lastID });
+  });
+});
+
+
+app.get('/getTasks', (req, res) => {
+  const sql = "SELECT * FROM tasks";
+
+  db.all(sql, [], (err, rows) => {
+    if (err) {
+      return res.status(500).send(err.message);
+    }
+    return res.status(200).send(rows);
+  });
+});
